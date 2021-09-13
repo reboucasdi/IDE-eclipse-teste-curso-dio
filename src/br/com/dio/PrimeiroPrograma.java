e86f078caa7b423a2b4918d8694e6a14f68c4bfe
reboucasdi <diegoreboucas.santana3@gmail.com>
2021-09-13 19:37:31 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		 Livros livros = new Livros();
+		
+		System.out.print(gato);
+		System.out.print(livros);
+		
+		// TODO Auto-generated method stub
+			
+	}
+
+}
+
+
+
+class Livros {
+	private String nome;
+	private String npag;
+}
